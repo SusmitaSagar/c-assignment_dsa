a1ba12bee86a4bfef76078f4727399dc51c0465f
SusmitaSagar_202124 <susmitasagar038@gmail.com>
2021-11-15 17:58:02 +0530

    Add files via upload

---
+++ swapcallvalue.cpp
@@ -0,0 +1,31 @@
+//Write a program for swapping two numbers 
+//using ‘call by value’ and ‘call by reference’ strategies.
+#include<iostream>
+using namespace std;
+
+void swap(int,int);
+
+int main()
+{
+        int a,b;
+        cout<<"Enter Value Of A :: ";
+        cin>>a;
+        cout<<"\nEnter Value of B :: ";
+        cin>>b;
+
+        cout<<"\nBefore Swapping, Value of :: \n\tA = "<<a<<"\tB = "<<b<<"\n";
+
+        swap(a,b);
+
+        cout<<"\nOutside Function After Swapping, Value of :: \n\tA = "<<a<<"\tB = "<<b<<"\n";
+}
+
+
+void swap(int a,int b)
+{
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        cout<<"\nInside Function After Swapping, Value of :: \n\tA = "<<a<<"\tB = "<<b<<"\n";
+}
